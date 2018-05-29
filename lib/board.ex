86ad1cd362d14553d5d2d67e962e36adcad70a00
akiraandy <andypschwartz@gmail.com>
2018-05-29 14:02:05 -0500

    add board module and board tests

---
+++ lib/board.ex
@@ -0,0 +1,34 @@
+defmodule Board do
+  @moduledoc """
+  Board for TTT
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Game.hello
+      :world
+
+  """
+  def place_marker(board, marker, space) do
+    List.replace_at(board, space - 1, marker)
+  end
+
+  def is_empty?(board) do
+    Enum.all?(board, fn(space) -> is_integer(space) end)
+  end
+
+  def available_spaces(board) do
+    Enum.filter(board, fn(space) -> is_integer(space) end)
+  end
+
+  def full?(board) do
+    Enum.all?(board, fn(space) -> is_atom(space) end)
+  end
+
+  def space_available?(board, space) do
+    is_integer(Enum.at(board, space - 1))
+  end
+end
